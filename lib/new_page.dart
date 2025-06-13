2f907bde1c06eaa1361a976c4731122b0d2319f0
sompong47 <maserxtv@gmail.com>
2025-06-13 15:24:40 +0700

    add

--- lib/new_page.dart
+++ lib/new_page.dart
@@ -11,8 +11,8 @@ class _MyNewPageState extends State<MyNewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('New Page')),
-      body: Text('data'),
+      appBar: AppBar(title: Text('GODJI APP')),
+      body: Center(child: Text("สวัสดีคนไทย"),),
     );
   }
 }
